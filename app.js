b0fb26f1ccdf9988ed87071c18ab2ced317d39d8
Monika <monika.jain.0903@gmail.com>
2024-03-25 23:01:35 +0530

    //changes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressSession=require("express-session");
 const flash=require("connect-flash");
+require('dotenv').config();
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,10 @@
-// const mongoose = require('mongoose');
-// const plm=require("passport-local-mongoose");
-
-// mongoose.connect("mongodb://127.0.0.1:27017/newapp");
-
-// const userSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String
-//   },
-//   posts: [{
-//     type:mongoose.Schema.Types.ObjectId,
-//     ref:"Post"
-//   }],
-//   dp: {
-//     type: String, // Assuming the profile picture is stored as a URL
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   fullName: {
-//     type: String,
-//     required: true,
-//   },
-// });
-
-// userSchema.plugin(plm);
-
-// module.exports= mongoose.model('User', userSchema);
-
-
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb+srv://admin123:admin123@cluster0.psewfip.mongodb.net/Pinterest");
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
 const userSchema = new mongoose.Schema({
