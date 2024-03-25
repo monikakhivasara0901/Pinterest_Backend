255cbe551816798597f2795af908e2019eaf0fb4
Monika <monika.jain.0903@gmail.com>
2024-03-25 17:21:46 +0530

    //MongoDb Atlas

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,45 @@
-const mongoose = require('mongoose');
-const plm=require("passport-local-mongoose");
+// const mongoose = require('mongoose');
+// const plm=require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/newapp");
+// mongoose.connect("mongodb://127.0.0.1:27017/newapp");
+
+// const userSchema = new mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String
+//   },
+//   posts: [{
+//     type:mongoose.Schema.Types.ObjectId,
+//     ref:"Post"
+//   }],
+//   dp: {
+//     type: String, // Assuming the profile picture is stored as a URL
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   fullName: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// userSchema.plugin(plm);
+
+// module.exports= mongoose.model('User', userSchema);
+
+
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect("mongodb+srv://admin123:admin123@cluster0.psewfip.mongodb.net/Pinterest");
+
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -13,11 +51,12 @@ const userSchema = new mongoose.Schema({
     type: String
   },
   posts: [{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Post"
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
   }],
   dp: {
-    type: String, // Assuming the profile picture is stored as a URL
+    type: String,
+    // Assuming the profile picture is stored as a URL
   },
   email: {
     type: String,
@@ -32,6 +71,4 @@ const userSchema = new mongoose.Schema({
 
 userSchema.plugin(plm);
 
-module.exports= mongoose.model('User', userSchema);
-
-
+module.exports = mongoose.model('User', userSchema);
